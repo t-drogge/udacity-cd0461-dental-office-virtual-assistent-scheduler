6a1b64439d237a8b35fe6c52a987e63252c8f71b
Thorsten Drogge <tdrogge@outlook.de>
2023-05-19 16:12:20 +0200

    Updated welcome message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", (req, res, next) => {
-    res.json({ message: "hello world" })
+    res.json({ message: "Welcome to the Contoso Dentistry Scheduler" })
 })
 
 app.get("/availability", (req, res, next) => {
